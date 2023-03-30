24592b2a451fa03e1aa34d6d9831bdc64929612f
Gabriel Lima <gabriel.lima4586@gmail.com>
2023-03-30 16:52:35 -0300

    Adição de gráficos

--- import matplotlib graficos.py
+++ import matplotlib graficos.py
@@ -3,10 +3,19 @@
 # ---------------------------------------------------------------------------- #
 
 # --- É possível criar gráficos com diversas libs utilizaremos a matplotlib -- #
-import matplotlib.pyplot as plt #definindo apelidos para libs
-x = list(range(1,9))
-y = nota_matematica
-plt.plot(x,y, marker = 'o')
-plt.xlabel('Provas')
-plt.ylabel('Notas')
-plt.show()
+import matplotlib.pyplot as plt
+
+notas_matematica = ['Matemática',8,7,6,6,7,7,8,10]
+notas_portugues = ['Português',9,9,9,8,5,6,8,5]
+notas_geografia = ['Geografia',10,10,6,7,7,7,8,7]
+
+notas = [notas_matematica, notas_portugues, notas_geografia]
+
+for nota in notas:
+ x = list(range(1, 9))
+ y = nota[1:]
+ plt.plot(x, y, marker='o')
+ plt.xlabel('Provas')
+ plt.ylabel('Notas')
+ plt.title(nota[0])
+ plt.show()
