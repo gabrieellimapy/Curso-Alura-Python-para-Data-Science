2b88b51592c69c85ab825f60fa5cef44b481dee2
Gabriel Lima <gabriel.lima4586@gmail.com>
2023-03-31 13:34:10 -0300

    adição novo módulo

---
+++ Trabalhando com listas/listas 1.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------- #
+#                          COMO CRIAR LISTAS NO PYTHON                         #
+# ---------------------------------------------------------------------------- #
+# Pode ser usado com um par de colchetes []
+# Utilizando um par de colchetes com itens separados por vírgulas [1,2,3,4,5,6]
+
+Acessorios = ['Rodas de Liga,','Travas Eletricas','Piloto automático', 'Bancos de couro', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva']
+Acessorios
+
+# ---------------------------------------------------------------------------- #
+#                  Criando listas com tipos de dados variados                  #
+# ---------------------------------------------------------------------------- #
+
+Carro_1 = ['Jetta Variant', 'Motor 4.0 Turbo', 2003, 44410.0, False,['Rodas de Liga', 'Travas elétricas', 'Piloto automático'], 85078.64]
+Carro_2 = ['Passat', 'Motor Diesel', 1991, 5712.0, False,['Central multimídia', 'Teto paronâmico','Freios ABS'],106161.94]
