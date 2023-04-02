ae2a1998858536f3e24b81ef0806320295ce4c2f
gabrieellimapy <gabriel.lima4586@gmail.com>
2023-04-02 13:58:09 -0300

    adição de loops aninhados, para entender melhor a didática do python

---
+++ Condicionais e Laços/loops aninhados.py
@@ -0,0 +1,36 @@
+# ---------------------------------------------------------------------------- #
+#                                Loops aninhados                               #
+# ---------------------------------------------------------------------------- #
+# ---------------- Instruçaõ for dentro de outra instrução for --------------- #
+dados = [ 
+    ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'],
+    ['Central multimídia', 'Teto panorâmico', 'Freios ABS', '4 X 4', 'Painel digital', 'Piloto automático', 'Bancos de couro', 'Câmera de estacionamento'],
+    ['Piloto automático', 'Controle de estabilidade', 'Sensor crepuscular', 'Freios ABS', 'Câmbio automático', 'Bancos de couro', 'Central multimídia', 'Vidros elétricos']
+]
+dados
+
+# ----------- Identificar quais acessórios temos dentro do data set ---------- #
+
+for lista in dados:
+    print(lista)
+    
+# Para varrer as três listas
+for lista in dados:
+    for item in lista:
+        print(item)
+
+# pegar apenas os itens individualmenmte
+
+Acessorios = []
+for lista in dados:
+    for item in lista:
+        Acessorios.append(item)
+Acessorios
+
+# ---------------------------------------------------------------------------- #
+#                                     SET()                                    #
+# ---------------------------------------------------------------------------- #
+
+set(Acessorios) 
+
+list (set(Acessorios))
