dc3ef85afac1093329ba76a90ae1244241a6e660
gabrieellimapy <gabriel.lima4586@gmail.com>
2023-04-03 13:13:07 -0300

    /* -------------------------------------------------------------------------- */ /*                                  INCLUSÃO                                  */ /* -------------------------------------------------------------------------- */
    Incluídos conteúdos de Elif e mais anotações sobre AND e OR

--- Condicionais e Laços/tipos de ifelse.py
+++ Condicionais e Laços/tipos de ifelse.py
@@ -30,3 +30,86 @@ for lista in dados:
 carrozero
 
 carrousado
+
+
+# ---------------------------------------------------------------------------- #
+#                                     ELIF                                     #
+# ---------------------------------------------------------------------------- #
+
+# ----------------------------- Formatação Padrão ---------------------------- #
+
+#if <condição 1>:
+    #<instruções caso a condição 1 seja verdadeira>
+#elif <condição 2>:
+    #<instruções caso a condição 2 seja verdadeira>
+#elif <condição 3>:
+    #<instruções caso a condição 3 seja verdadeira>
+   #                     .
+   #                     .
+   #                     .
+#else:
+    #<instruções caso as condições anteriores não sejam verdadeiras>
+    
+dados
+
+# ------------------------- Separar veículos por anos ------------------------ #
+
+A, B, C = [], [], []
+for lista in dados:
+    if(lista[1] <= 2000):
+        A.append(lista)
+    elif(lista[1] > 2000 and lista[1]<= 2010):
+        B.append(lista)
+    else:
+        C.append(lista)
+        
+A
+B
+C
+
+# ---------------------------------------------------------------------------- #
+#                      Maneira mais simples de usar o elif                     #
+# ---------------------------------------------------------------------------- #
+
+A, B, C = [], [], []
+for lista in dados:
+    if(lista[1] <= 2000):
+        A.append(lista)
+    elif(2000 < lista[1]<= 2010):
+        B.append(lista)
+    else:
+        C.append(lista)
+
+A
+B
+C
+
+
+# ---------------------------------------------------------------------------- #
+#                                 OPERADOR AND                                 #
+# ---------------------------------------------------------------------------- #
+
+print('AND')
+print(f'(True and True) o resultado é: {True and True}')
+# -------------------------------- Saída= True ------------------------------- #
+print(f'(True and False) o resultado é: {True and False}')
+# ------------------------------- Saída= False ------------------------------- #
+print(f'(False and True) o resultado é: {False and True}')
+# ------------------------------- Saída= False ------------------------------- #
+print(f'(False and False) o resultado é: {False and False}')
+# ------------------------------- Saída: False ------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+#                                  OPERADOR OR                                 #
+# ---------------------------------------------------------------------------- #
+
+print('OR')
+print(f'(True or True) o resultado é: {True or True}')
+# ------------------------------- Saída = True ------------------------------- #
+print(f'(True or False) o resultado é: {True or False}')
+# ------------------------------- Saída = True ------------------------------- #
+print(f'(False or True) o resultado é: {False or True}')
+# ------------------------------- Saída = True ------------------------------- #
+print(f'(False or False) o resultado é: {False or False}')
+# ------------------------------- Saída = False ------------------------------ #
