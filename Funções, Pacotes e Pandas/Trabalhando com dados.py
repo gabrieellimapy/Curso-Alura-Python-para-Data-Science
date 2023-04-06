18a1f88ba1e3ca57d348a9c9524881471c9d3e34
Gabriel Lima <gabriel.lima4586@gmail.com>
2023-04-06 15:00:32 -0300

    /* -------------------------------------------------------------------------- */ /*                             ATUALIZAÇÃO V 5.5.1                            */ /* -------------------------------------------------------------------------- */
    Adição de novo curso da Alura, darei continuidade hoje a noite a esta parte

---
+++ Funções, Pacotes e Pandas/Trabalhando com dados.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------- #
+#                        Trabalhando com dados em Python                       #
+# ---------------------------------------------------------------------------- #
+
+# ----------------------------- Importando pandas ---------------------------- #
+import pandas as pd
+
+database = pd.read_csv('Funções, Pacotes e Pandas\data\db.csv', sep = ';') 
+database
