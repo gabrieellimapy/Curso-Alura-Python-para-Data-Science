9604edb89a03b3eb1c4ffb9ff62c939d763f6a2e
gabrieellimapy <gabriel.lima4586@gmail.com>
2023-04-06 20:46:56 -0300

    /* -------------------------------------------------------------------------- */ /*                             ATUALIZAÇÃO V.5.5.2                            */ /* -------------------------------------------------------------------------- */
    Adição de mais conteúdo sobre pandas e como obter listas em CSV

--- Funções, Pacotes e Pandas/Trabalhando com dados.py
+++ Funções, Pacotes e Pandas/Trabalhando com dados.py
@@ -4,6 +4,15 @@
 
 # ----------------------------- Importando pandas ---------------------------- #
 import pandas as pd
-
-database = pd.read_csv('Funções, Pacotes e Pandas\data\db.csv', sep = ';') 
+pd.set_option('display.max_rows', 1000) #mostra o máximo de informações na tela
+# ------------------- Para ver todas as linhas do database ------------------- #
+pd.set_option('display.max_columns', 1000) #mostra o máximo de colunas na tela
+# ------------------- Para ver todas as colunas do database ------------------ #
+database = pd.read_csv('Funções, Pacotes e Pandas\data\db.csv', sep = ';') # ; separador usado no dataset
 database
+# --------------- Comando de leitura de arquivos CSV em pandas --------------- #
+database.dtypes
+# ------------------ Demonstra os tipos de dados da database ----------------- #
+
+database[['Quilometragem', 'Valor']].describe()
+# --------------- Gera um conjunto de estatísticas descritivas --------------- #
