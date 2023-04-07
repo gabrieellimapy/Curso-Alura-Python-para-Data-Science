4c028a97596a252d8ee3ec701e501831ac2cd276
gabrieellimapy <gabriel.lima4586@gmail.com>
2023-04-06 21:08:38 -0300

    /* -------------------------------------------------------------------------- */ /*                             ATUALIZAÇÃO V 5.5.3                            */ /* -------------------------------------------------------------------------- */
    Autualizado com novo módulo do terceiro curso de python da Alura

--- Funções, Pacotes e Pandas/Trabalhando com dados.py
+++ Funções, Pacotes e Pandas/Trabalhando com dados.py
@@ -18,3 +18,26 @@ database[['Quilometragem', 'Valor']].describe()
 # --------------- Gera um conjunto de estatísticas descritivas --------------- #
 database.info()
 # -------------- Demonstra as informações sobre a base de dados -------------- #
+
+# ---------------------------------------------------------------------------- #
+#                                CRIANDO TUPULAS                               #
+# ---------------------------------------------------------------------------- #
+
+# ----- Tupulas são sequencias imutáveis para armazenar coleções de itens ---- #
+# ------ geralmente heterogeneos podem ser constituídas de várias formas ----- #
+# ------------------------- Utilizando paranteses () ------------------------- #
+# -------------------- Utilizando uma vírgula a direita x, ------------------- #
+# - Utilizando um par de parenteses e itens separados por virgulas (x, y, z) - #
+# ------------------ Utilizando: tuple() ou tuple(iterador) ------------------ #
+
+# --------------------- Criando uma tupula com variáveis --------------------- #
+nome = 'Passat'
+valor = 153000
+(nome, valor)
+
+# --------------- Criando uma tupula com valores pré definidos --------------- #
+
+nomes_carros = tuple(['Jetta Variant', 'Passat', 'Crossfox,', 'Dss'])
+
+# ----------- Como ver o tipo de um dado no python (retorna tuple) ----------- #
+type(nomes_carros)
