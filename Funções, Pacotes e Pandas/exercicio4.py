0e221ac9528e68983a6befceb189edfb4ef562e9
Gabriel Lima <gabriel.lima4586@gmail.com>
2023-04-10 15:23:12 -0300

    /* -------------------------------------------------------------------------- */ /*                             ATUALIZAÇÃO V.5.5.6                            */ /* -------------------------------------------------------------------------- */
    Adicionando mais conteúdos sobre bibliotecas

---
+++ Funções, Pacotes e Pandas/exercicio4.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------------------------- #
+#              DESAFIO - FORMAS SIMPLES DE CRIAÇÃO DE DICIONÁRIOS              #
+# ---------------------------------------------------------------------------- #
+
+# -- Vimos em nossa última aula algumas maneiras de se criar um dicionário. -- #
+
+# ------------------ Tendo como base as duas listas abaixo: ------------------ #
+
+nomes = ['Passat', 'Crossfox', 'DS5', 'C4', 'Jetta']
+kms = [15000, 12000, 32000, 8000, 50000]
+
+dict(zip(nomes,kms))
