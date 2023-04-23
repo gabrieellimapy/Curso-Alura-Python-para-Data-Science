1007bf2b9c6e6a389a40ce55e265cb3eb1d1f9e6
gabrieellimapy <gabriel.lima4586@gmail.com>
2023-04-23 11:10:35 -0300

    /* ----------------------------- UPDATE V.2.5.8 ----------------------------- */
    Inclusão de novos exercícios e aplicações com DataFrames e Series, como a utilizado de .loc e .iloc

--- Curso Pandas/Mini curso pandas.py
+++ Curso Pandas/Mini curso pandas.py
@@ -61,3 +61,85 @@ dataset2 = pd.DataFrame(dados)
 
 dataset3 = pd.read_csv('Curso Pandas/data/db.csv', sep=';')
 dataset3
+
+
+# ---------------------------------------------------------------------------- #
+#                            SELEÇÕES COM DATAFRAMES                           #
+# ---------------------------------------------------------------------------- #
+
+# --------------------------- Selecionando Colunas --------------------------- #
+
+dataset3['valor']
+
+type(dataset3['valor'])
+
+# ---------- Cada coluna de um DataFrame pode ser tatada como Series --------- #
+
+dataset3[['Valor']]  # Retorna uma única coluna, como um Dataframe
+
+type(dataset3[['Valor']])
+
+
+# ---------------------------------------------------------------------------- #
+#                          Selecionando linhas [i : j]                         #
+# ---------------------------------------------------------------------------- #
+
+
+# -------- A indexação tem origem no zero e nos fatiamentos (*slices*) ------- #
+# -------- a linha com índice i é **incluída** e a linha com índice j -------- #
+# --------------------- **não é incluída** no resultado. --------------------- #
+
+dataset3[0 : 3] # De zero ao terceiro item
+
+# ---------------------------------------------------------------------------- #
+#                                     .LOC                                     #
+# ---------------------------------------------------------------------------- #
+
+# ------------------ Seleciona um grupo de linhas e colunas ------------------ #
+# ---------------- seguindo os rótulos ou uma matriz booleana ---------------- #
+
+dataset3.loc['Passat']
+
+# ----------- Retorna uma series com todas as informações do Passat ---------- #
+
+dataset3.loc[['Passat','DS5']]
+
+# -------- Retorna duas linhas sobre o Passat e DS5 como um DataFrame -------- #
+
+dataset3.loc[['Passat', 'DS5'], ['Motor', 'Valor']]
+
+# ----------- Retorna o mesmo DataFrame, com informações filtradas ----------- #
+
+dataset3.loc[:, ['Motor', 'Valor']]
+
+# ------- Retorna o valor das duas colunas para todo o DataFrame gerado ------ #
+
+# ---------------------------------------------------------------------------- #
+#                                     .ILOC                                    #
+# ---------------------------------------------------------------------------- #
+
+# ----------------- Seleciona com base nos índices, ou seja, ----------------- #
+# -------------------- se baseia na posição da informação -------------------- #
+
+dataset3.iloc[1]
+
+# --------------------- Seleciona a informação do carro 1 -------------------- #
+# ----------------------------- Retorna em Series ---------------------------- #
+
+dataset3.iloc[[1]]
+
+# --------------------------- Retorna em DataFrame --------------------------- #
+
+dataset3.iloc[1:4]
+
+# -------------- Busca do primeiro índice até o terceiro índice -------------- #
+# -------- Seguindo o conceito de inclui um e diminui o valor do final ------- #
+# --------------------------- Retorna em DataFrame --------------------------- #
+
+dataset3.iloc[1:4. [0, 5, 2]]
+
+dataset3.iloc[1:4, [0, 32, 52]]
+
+# -- Retorna um DataFrame com as informações dos carros e colunas filtradas -- #
+
+
--- Curso Pandas/exercicio2.py
+++ Curso Pandas/exercicio2.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 dados = {
-    'Nome': ['Jetta', 'Passat', 'Crossfox', 'DS5', 'Fusca'], 
     'Motor': ['Motor 4.0 Turbo', 'Motor Diesel', 'Motor Diesel V8', 'Motor 2.0', 'Motor 1.6'],
     'Ano': [2019, 2003, 1991, 2019, 1990],
     'Quilometragem': [0.0, 5712.0, 37123.0, 0.0, 120000.0],
@@ -9,6 +8,8 @@ dados = {
     'Valor': [88000.0, 106000.0, 72000.0, 89000.0, 32000.0]
 }
 
-dataset = pd.DataFrame(dados)
+dataset = pd.DataFrame(dados, index = ['Jetta', 'Passat', 'Crossfox', 'DS5', 'Fusca'])
 
-dataset[['Nome', 'Ano', 'Quilometragem', 'Valor']][1:3]
+dataset.loc[['Passat','DS5'], ['Motor','Valor']]
+
+dataset.iloc[[1,3], [0, -1]]
